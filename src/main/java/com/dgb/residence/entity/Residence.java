415e38a2ca01abc0c164c390a48854368a649ef6
LSH <lsh2613@naver.com>
2024-12-08 23:58:38 +0900

    [feat] @Enumerated(EnumType.STRING) 추가

--- src/main/java/com/dgb/residence/entity/Residence.java
+++ src/main/java/com/dgb/residence/entity/Residence.java
@@ -3,6 +3,8 @@ package com.dgb.residence.entity;
 
 import com.dgb.constant.DealType;
 import com.dgb.constant.ResidenceType;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.MappedSuperclass;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -16,7 +18,9 @@ import java.time.YearMonth;
 @NoArgsConstructor
 @AllArgsConstructor
 public abstract class Residence {
+    @Enumerated(EnumType.STRING)
     protected ResidenceType residenceType;
+    @Enumerated(EnumType.STRING)
     protected DealType dealType;
     protected YearMonth dealDate;
     protected Point point;
