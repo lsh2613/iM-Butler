b610431b0214109969dbcf92f3ea2197fe7ea945
LSH <lsh2613@naver.com>
2024-12-09 02:23:39 +0900

    [feat] 주소 변환 실패에 대한 예외 처리 및 로깅 추가

--- src/main/java/com/dgb/navermaps/service/NaverMapsService.java
+++ src/main/java/com/dgb/navermaps/service/NaverMapsService.java
@@ -2,6 +2,8 @@ package com.dgb.navermaps.service;
 
 import com.dgb.navermaps.util.NaverMapsApiUtil;
 import com.dgb.residence.entity.ResidenceAddress;
+import lombok.extern.slf4j.Slf4j;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.GeometryFactory;
@@ -13,6 +15,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+@Slf4j
 @Service
 public class NaverMapsService {
 
@@ -36,13 +39,20 @@ public class NaverMapsService {
                 httpEntity,
                 String.class
         );
+        ;
 
-        JSONObject addresses = new JSONObject(response.getBody())
-                .getJSONArray("addresses")
-                .getJSONObject(0);
+        JSONArray addresses = new JSONObject(response.getBody())
+                .getJSONArray("addresses");
 
-        double x = addresses.getDouble("x");
-        double y = addresses.getDouble("y");
+        if (addresses.length() == 0) {
+            log.debug("{}에 대한 주소가 좌표 변환에 실패하여 (0, 0)으로 저장되었습니다", residenceAddress.mapToString());
+            return new GeometryFactory().createPoint(new Coordinate(0, 0));
+        }
+
+        JSONObject address = addresses.getJSONObject(0);
+
+        double x = address.getDouble("x");
+        double y = address.getDouble("y");
 
         return new GeometryFactory().createPoint(new Coordinate(y, x));
     }
