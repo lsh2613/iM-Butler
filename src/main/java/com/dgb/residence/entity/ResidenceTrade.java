af29dd8069b0495fa6c5893e7d94530bdcf37902
LSH <lsh2613@naver.com>
2024-12-14 21:04:42 +0900

    [feat] ResidenceTrade에 @MappedSuperclass 추가

--- src/main/java/com/dgb/residence/entity/ResidenceTrade.java
+++ src/main/java/com/dgb/residence/entity/ResidenceTrade.java
@@ -1,8 +1,10 @@
 package com.dgb.residence.entity;
 
 
+import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 
+@MappedSuperclass
 @Getter
 public abstract class ResidenceTrade extends Residence {
     protected Integer dealAmount;
