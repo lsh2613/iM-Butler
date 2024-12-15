b0d47a070942b2858e3c348f1f21bd671a0033a9
LSH <lsh2613@naver.com>
2024-12-15 23:47:21 +0900

    [chore] @Getter 추가

--- src/main/java/com/dgb/constant/LegalDistrictCode.java
+++ src/main/java/com/dgb/constant/LegalDistrictCode.java
@@ -1,5 +1,8 @@
 package com.dgb.constant;
 
+import lombok.Getter;
+
+@Getter
 public enum LegalDistrictCode {
     // 대구
     군위군("27720"), 남구("27200"), 달서구("27290"), 달성군("27710"), 동구("27140"), 북구("27230"), 서구("27170"), 수성구("27260"), 중구("27110");
