04498472dbc30f5a312a21024e70245a0173e129
LSH <lsh2613@naver.com>
2024-12-16 19:44:36 +0900

    [mod] schema.sql -> @Column(columnDefinition..)으로 변경

--- src/main/java/com/dgb/residence/entity/Residence.java
+++ src/main/java/com/dgb/residence/entity/Residence.java
@@ -1,6 +1,7 @@
 package com.dgb.residence.entity;
 
 
+import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.locationtech.jts.geom.Point;
@@ -11,6 +12,7 @@ import java.time.YearMonth;
 @Getter
 public abstract class Residence {
     protected YearMonth dealDate;
+    @Column(columnDefinition = "POINT NOT NULL SRID 4326")
     protected Point point;
 
     public abstract void update(Residence newResidence);
